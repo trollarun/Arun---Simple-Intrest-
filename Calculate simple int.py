e35d43b29c435010ff4722122eab4aa077a9e914
trollarun <58756690+trollarun@users.noreply.github.com>
2019-12-11 07:55:38 +0530

    Python Code files uploaded

---
+++ Calculate simple int.py
@@ -0,0 +1,20 @@
+#Calculate Simple Intrest
+
+import math
+
+
+#Input
+#Set variables
+p = float (input("Mention the principal amount "))
+r = float   (input  ("Mentionn the rate of intrest in percentage "))
+t = float   (input  ("Mention the time period "))
+
+#processing
+#formual p*t*r / 100
+#check in modules for simple intrest
+
+si = p*t*r / 100
+
+
+#output
+print(si)
